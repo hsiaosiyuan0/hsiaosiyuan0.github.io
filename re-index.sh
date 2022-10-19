1b17a425c9a1545b20fcd19728e65db3992b2320
hsiaosiyuan0 <hsiaosiyuan0@gmail.com>
2022-10-20 01:16:13 +0800

    feat: style updates

--- re-index.sh
+++ re-index.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it --env-file=.env -e "CONFIG=$(cat config.json | jq -r tostring)" algolia/docsearch-scraper
+docker run -it --env-file=./docsearch/.env -e "CONFIG=$(cat ./docsearch/config.json | jq -r tostring)" algolia/docsearch-scraper
